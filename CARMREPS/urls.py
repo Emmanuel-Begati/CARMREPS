562a296a96a36eea9d843feaadb5dd5f29c7e79e
Emmanuel-Begati <e.yakubu@alustudent.com>
2023-10-30 14:16:06 +0200

    finished setting up django app

--- CARMREPS/urls.py
+++ CARMREPS/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from . import website as website_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
